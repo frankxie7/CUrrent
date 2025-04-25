327c9301f287891e33c01f256e71cfe47e718640
Frank Xie <fx58@cornell.edu>
2025-04-25 12:45:28 -0400

    Set up stuff

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,17 @@
-from flask import Flask
-from flask_cors import CORS
-
+from db import db, Course, User, Assignment
+from flask import Flask, request
+import json
 
 app = Flask(__name__)
-CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from models.models import db
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_filename}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
 db.init_app(app)
+with app.app_context():
+    db.create_all()
 
-app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(listing_bp, url_prefix='/listings')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
